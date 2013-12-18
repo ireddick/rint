0b98da1de7ec4ab407335c80e6f5c01d62e33f43
Iain Reddick <iain.reddick@gmail.com>
2013-12-18 15:20:49 +0000

    remove unneeded jasmine conf

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,4 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    jasmine_node: {
-      specNameMatcher: "spec",
-      projectRoot: ".",
-      requirejs: false,
-      forceExit: true,
-      jUnit: {
-        report: false,
-        savePath : "./build/reports/jasmine/",
-        useDotNotation: true,
-        consolidate: true
-      }
-    }
-  });
-
   grunt.loadNpmTasks('grunt-jasmine-node');
 
   grunt.registerTask('test', 'jasmine_node');
