ba6edfae6037a8d8727f6ce09f054f009dc8aa66
Iain Reddick <iain.reddick@gmail.com>
2013-12-18 06:06:33 +0000

    add core #times functionality

--- spec/rint_spec.js
+++ spec/rint_spec.js
@@ -1,5 +1,10 @@
 var RInt = function(n) { this.n = n }
+
 RInt.prototype.toInt = function() { return this.n }
+
+RInt.prototype.times = function(fn) {
+  for (var i = 0; i < this.n; i++) { fn(i) }
+}
 
 var rint = function(n) { return new RInt(n) }
 
@@ -19,4 +24,32 @@ describe("RInt", function() {
 
   })
 
+  describe("#times", function() {
+
+    var fn
+
+    beforeEach(function() {
+      fn = jasmine.createSpy("fn")
+    })
+
+    it("calls the given function n times", function() {
+      rint(3).times(fn)
+
+      expect(fn.calls.length).toEqual(3)
+    })
+
+    it("does not call the given function when n is negative", function() {
+      rint(-3).times(fn)
+
+      expect(fn).not.toHaveBeenCalled()
+    })
+
+    it("passes each value from 0 to n-1", function() {
+      var values = []
+      rint(3).times(function(n) { values.push(n) })
+
+      expect(values).toEqual([0, 1, 2])
+    })
+
+  })
 })
